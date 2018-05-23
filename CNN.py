0e62c512455a631b26160f6dcbaef8e2016e2264
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-05-23 14:58:17 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import keras
 from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, MaxPooling2D
 
