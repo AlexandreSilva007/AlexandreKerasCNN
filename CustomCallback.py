ffd995ec51c54ca06b0d5ad3ddb231e8c862ca2f
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-05-24 12:15:05 -0400

    Update CustomCallback.py

--- CustomCallback.py
+++ CustomCallback.py
@@ -5,10 +5,10 @@ class CustomCallback(keras.callbacks.Callback):
     print("Go!!!!!!")
     
   def progressBar(self, n):
-    print("\rProgresso: %s %s%s" % ("■"*n,n,'%'), end=" ", flush=False)
+    print("\rProgresso: %s %s%s" % ("■"*n,n,'%'), end="", flush=True)
 
   def on_epoch_begin(self, epoch, logs = None):
-    print("\rComecando época ",epoch, end=" ")
+    print("\rComecando época ",epoch, end=" ", flush=True)
     self._batch_percentage_count = 0
         
   def on_batch_begin(self, batch, logs = None):
@@ -16,10 +16,10 @@ class CustomCallback(keras.callbacks.Callback):
     self.progressBar( int(self._batch_percentage_count) )
   
   def on_batch_end(self, batch, logs = None):
-    pass
+    print("\r",end="", flush=True)
   
   def on_epoch_end(self, epoch, logs={}):
-    print("\rFim da época ",epoch, "\tacc: ", logs.get('acc'), "\ttest_acc: ", logs.get('val_acc'), "\terro:", logs.get('loss'))
+    print("\rEpoca ",epoch, "\tacc: ", logs.get('acc'), "\ttest_acc: ", logs.get('val_acc'), flush=True)#, "\terro:", logs.get('loss'))
    
   def on_train_end(self, logs = None):
     print("Finish!!!")
