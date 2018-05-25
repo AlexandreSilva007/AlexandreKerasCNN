6ae762248069a790490991980c989f474828f0ef
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-05-24 21:09:57 -0400

    Update CustomCallback.py

--- CustomCallback.py
+++ CustomCallback.py
@@ -4,6 +4,8 @@ import matplotlib.pylab as plt
 class CustomCallback(keras.callbacks.Callback):
   def on_train_begin(self, logs={}):
     print("Go!!!!!!")
+    self.hist_train = []
+    self.hist_test = []
     
   def on_epoch_begin(self, epoch, logs = None):
     print("\rComecando época ",epoch, end=" ", flush=True)
@@ -23,21 +25,30 @@ class CustomCallback(keras.callbacks.Callback):
   
   def on_epoch_end(self, epoch, logs={}):
     print("\rEpoca ",epoch, "\tacc: ", logs.get('acc'), "\ttest_acc: ", logs.get('val_acc'), flush=True)#, "\terro:", logs.get('loss'))
-    fig=plt.figure(figsize=(10,5))
-    fig.add_subplot(1, 2, 1)
+    self.hist_train.append(logs.get('val'))
+    self.hist_test.append(logs.get('val_acc'))
+    fig=plt.figure(figsize=(15,5))
+    fig.add_subplot(1, 3, 1)
     plt.grid(True)
     plt.plot(self.acc)
     plt.plot(self.val_acc)
     plt.xlabel('Batch')
     plt.ylabel('Valor')
     plt.legend(['Treino', 'Validacao'], loc='upper left')
-    fig.add_subplot(1, 2, 2)
+    fig.add_subplot(1, 3, 2)
     plt.bar(1, logs.get('acc'), 0.8)
     plt.bar(2, logs.get('val_acc'), 0.8)
     plt.bar(3, logs.get('loss'), 0.8)
     plt.xlabel('Elementos')
     plt.ylabel('Precisao')
     plt.legend(['Treino', 'Validacao','Erro'], loc='upper left')
+    fig.add_subplot(1, 3, 3)
+    plt.grid(True)
+    plt.plot(self.hist_train)
+    plt.plot(self.hist_test)
+    plt.xlabel('Epoca')
+    plt.ylabel('Precisao')
+    plt.legend(['Treino', 'Validacao'], loc='upper left')
     plt.show()
     
   def on_train_end(self, logs = None):
