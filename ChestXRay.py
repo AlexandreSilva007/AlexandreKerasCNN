7e726375e128f210e348fff85f1e254b60fe69ba
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-06-04 11:34:18 -0400

    Update ChestXRay.py

--- ChestXRay.py
+++ ChestXRay.py
@@ -107,7 +107,7 @@ class ChestXRay(CNN):
 						img_file = np.asarray(img_file)
 						if img_file is not None:
 							img_file = skimage.transform.resize(img_file, (150, 150, 3))
-							img_file = img_file[15:135,30:120]#.crop((7, 12, 163, 138)) #crop
+							img_file = img_file[1:150,30:120]#.crop((7, 12, 163, 138)) #crop
 							X.append(img_file)
 							y.append(label)
 						else:
@@ -139,7 +139,7 @@ class ChestXRay(CNN):
 			featurewise_center=False,  # set input mean to 0 over the dataset
 			samplewise_center=False,  # set each sample mean to 0
 			featurewise_std_normalization=False,  # divide inputs by std of the dataset
-			samplewise_std_normalization=False,  # divide each input by its std
+			samplewise_std_normalization=True,  # divide each input by its std
 			zca_whitening=False,  # apply ZCA whitening
 			rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
 			width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
