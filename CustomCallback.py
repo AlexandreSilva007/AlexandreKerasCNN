3357542917b4b11e4eae5a7175ce59d50bb88597
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-06-06 16:24:40 -0400

    Update CustomCallback.py

--- CustomCallback.py
+++ CustomCallback.py
@@ -64,6 +64,7 @@ class CustomCallback(keras.callbacks.Callback):
     print ("\r f1_score: %s" % (_val_f1))
         
     if(self._epoch_percentage_count%self._epoch_step==0):
+      print("\r\n******************** RESULTADO PARCIAL ***********************")
       #confusion matrix
       cm = confusion_matrix(val_targ.argmax(axis=1), val_predict.argmax(axis=1))
       CustomCallback.plot_confusionmatrix2(cm, self._CONFUSION_LABELS)
@@ -127,28 +128,3 @@ class CustomCallback(keras.callbacks.Callback):
     plt.xticks(range(width), alphabet[:width])
     plt.yticks(range(height), alphabet[:height])
     #plt.show()
-
-
-    
-  def plot_confusion_matrix(cm, classes, normalize=False, title='Matriz de Confusão', cmap=plt.cm.Blues):
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Matriz de Confusão Normalizada")
-    else:
-        print('Matriz de Confusão, sem normalização')
-    print(cm)
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
